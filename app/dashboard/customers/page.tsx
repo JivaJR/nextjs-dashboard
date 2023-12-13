4e88f4e3c0e81044a1b4367f5c7787679566ae6d
JivaJr <ajavierr@uninorte.edu.co>
2023-12-13 11:16:24 -0500

    First Comit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+export default function CostumersPage()  {
+    return (
+        <p>Dashboar asdad page :heart</p>
+    )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import '@/app/ui/global.css';
+import { montserrat } from './ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${montserrat.className} antialiased`}>
+        {children}
+        <footer className='py-10 flex justify-center items-center'>
+          Esto esta realizadon con Next.js
+        </footer>
+      </body>
     </html>
   );
 }
